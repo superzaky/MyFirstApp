76544ee3b358eaa738edc974215720767553840c
superzaky <superkytoz@gmail.com>
2015-10-29 01:32:05 +0100

    supporting different screens - Create Different Layouts

--- app/src/main/java/com/example/yomac_000/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/yomac_000/myfirstapp/DisplayMessageActivity.java
@@ -15,14 +15,21 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // setContentView(R.layout.activity_display_message);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
-        // Create the text view
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
-        // Set the text view as the activity layout
-        setContentView(textView);
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+//        // Create the text view
+//        TextView textView = new TextView(this);
+//        textView.setTextSize(40);
+//        textView.setText(message);
+//        // Set the text view as the activity layout
+//        setContentView(textView);
 
+
+        // Get a string resource from your app's Resources
+        String hello = getResources().getString(R.string.hello_world);
+        // Or supply a string resource to a method that requires a string
+        TextView textView = new TextView(this);
+        textView.setText(R.string.hello_world);
+        setContentView(textView);
     }
 }
